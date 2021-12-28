98dcf0d2875bb294f5789b9677cef08c9181d2d2
Mohamed Hamdy <mohamedhamdy5281@gmail.com>
2021-12-28 20:46:15 +0200

    delete data base file

--- scripts/dropdb.sh
+++ scripts/dropdb.sh
@@ -1,1 +1,14 @@
 #! /bin/bash
+echo "Enter your database name: "
+read db_name;
+
+if [ -d ./database/$db_name ]
+then 
+    rm -r ./database/$db_name
+    echo -e "$db_name DATABASE is deleted..."
+    . ./main.sh
+else
+    echo -e "${ERRORTYPE}$db_name DATABASE didn't exist${NE}"
+    . ./main.sh
+    
+fi
