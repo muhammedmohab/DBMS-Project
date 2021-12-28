bf04e69eb516d92113d77e03565abf49f0595b75
muhammedmohab <muhammed.mohab@gmail.com>
2021-12-28 17:18:00 +0200

    Edited the functionallity of *createdb.sh* file

--- scripts/createdb.sh
+++ scripts/createdb.sh
@@ -5,12 +5,11 @@ read db_name;
 if [ -d ./database/$db_name ]
 then 
     echo "$db_name DATABASE exist"
-    cd ./database/$db_name
-    echo "You are in $db_name DATABASE"
-    PS3="DBMS($db_name) -> "
-       ./sqldb.sh
+    . ./main.sh
 else
     mkdir ./database/$db_name
     echo "$db_name Database created";
-    exit;
+    cd ./database/$db_name
+    echo "Using $db_name"
+    . ../../scripts.sqldb.sh
 fi
