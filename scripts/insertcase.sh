a0f48233e6d2a102e4961370d47311a9a0562e56
muhammedmohab <muhammed.mohab@gmail.com>
2021-12-30 12:57:34 +0200

    Added an alternative path in *insertcase.sh* file

--- scripts/insertcase.sh
+++ scripts/insertcase.sh
@@ -2,26 +2,32 @@
 echo "Enter table name" 
 read table_name;
 
-line=`cat ${table_name}.csv | head -1` #Reading the first line which has the cols
-IFS=","; read -ra items <<< "$line"
+if [ -f "$table_name" ]
+then
+	line=`cat ${table_name}.csv | head -1` #Reading the first line which has the cols
+    IFS=","; read -ra items <<< "$line"
 
-numberOfCols=${#items[@]} #number of cols to loop on
-counter=0
-while [ $numberOfCols -gt 0 ]
-do
-    echo "${items[$counter]}"
-    read value
-    echo -n $value >> ${table_name}.csv
-    if [ $numberOfCols -ne 1 ]
-    then
-        echo -n , >> ${table_name}.csv
-    fi
-    
-    if [ $numberOfCols -eq 1 ]
-    then 
-        echo "">> ${table_name}.csv
-    fi 
-    ((numberOfCols=$numberOfCols-1))
-    ((counter=$counter+1))
+    numberOfCols=${#items[@]} #number of cols to loop on
+    counter=0
+    while [ $numberOfCols -gt 0 ]
+    do
+        echo "${items[$counter]}"
+        read value
+        echo -n $value >> ${table_name}.csv
+        if [ $numberOfCols -ne 1 ]
+        then
+            echo -n , >> ${table_name}.csv
+        fi
+        
+        if [ $numberOfCols -eq 1 ]
+        then 
+            echo "">> ${table_name}.csv
+        fi 
+        ((numberOfCols=$numberOfCols-1))
+        ((counter=$counter+1))
 done
     cd ../.. #Taking the path to the main script path
+else
+    echo -e "${ERRORTYPE}Table is not found{$NE}";
+    cd ../.. #Taking the path to the main script path
+fi
