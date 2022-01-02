9400e0d4a0a1a6001b98aaa959e776f321c89798
muhammedmohab <muhammed.mohab@gmail.com>
2022-01-02 20:34:28 +0200

    Edited and commented the createtable.sh

--- scripts/createtable.sh
+++ scripts/createtable.sh
@@ -1,29 +1,35 @@
 #! /bin/bash
 i=1
+
 echo "Enter table name" 
 read table_name;
+
 if [[ -f $table_name ]] # if file dont exist then continue "${table_name}.csv" 
 then
-echo this TABLE already exist
+  echo "$table_name already exists"
 else 
-touch $table_name
-echo enter the Number of COLUMNS
-read number
- while [[ number -gt 0 ]]
- do 
- echo "enter the column number $i"
- read column 
- echo -n $column >> $table_name
-   if [[ $number -ne 1 ]]
-   then
-   echo -n , >> $table_name
-   fi
+  
+  touch $table_name
+  echo "Enter the number of columns"
+  read number
+  
+  while [[ number -gt 0 ]]
+  do 
+    echo "enter the column number $i"
+    read column
 
-   if [[ $number -eq 1 ]]
-   then 
-   echo "">> $table_name
-   fi
-   ((number=$number-1))
-   ((i=$i+1))
- done
+    echo -n $column >> $table_name
+
+      if [[ $number -ne 1 ]]
+      then
+        echo -n , >> $table_name
+      fi
+
+      if [[ $number -eq 1 ]]
+      then 
+        echo "">> $table_name
+      fi
+      ((number=$number-1))
+      ((i=$i+1))
+  done
 fi
