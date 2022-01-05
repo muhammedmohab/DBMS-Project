02bf66c3cc345605c60f4a1c103ce70d11886219
muhammedmohab <muhammed.mohab@gmail.com>
2022-01-06 00:59:27 +0200

    Commenting the select function TO BE FIXED

--- scripts/selecttable.sh
+++ scripts/selecttable.sh
@@ -1,1 +1,36 @@
-#! /bin/bash
+# #! /bin/bash
+
+# while [[ true ]]
+# do
+#     select selectiontype in "Select all" "Select col" "Select with condition" "Back"
+#     do 
+#         case selectiontype in 
+#         "Select all")
+#                     break;
+#                     ;;
+#         "Select col"
+#                     echo "Enter table name";
+#                     read table_name;
+#                     echo "Enter column name";
+#                     read col_name;
+
+#                     awk 'BEGIN{FS=","}
+#                     {
+#                         if(NR==1){
+#                             for(i=1;i<=NF;i++)
+#                             {
+#                                 if($i=="${col_name}")
+#                                 print i;
+#                             }
+#                         }
+#                     }' ${table_name}.csv
+#                     break;
+#                     ;;
+#         "Select with condition"
+#                     echo "Enter field name";
+#                     read table_name;
+#                     echo "select what col";
+#                     read col_name;
+#                     echo "Where ${col_name} equals to what";
+#                     read cond;
+
