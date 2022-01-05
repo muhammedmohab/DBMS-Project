4a2a0a0b82f7c458394802939b39f323a967e4db
muhammedmohab <muhammed.mohab@gmail.com>
2022-01-06 01:03:38 +0200

    Editing the delete function

--- scripts/deletefromsql.sh
+++ scripts/deletefromsql.sh
@@ -1,9 +1,30 @@
 #! /bin/bash
 
-where=$(cat ${line[2]}.csv | grep -n ${line[6]} | cut -d: -f1);
-if [[ $where != 1 ]]
+# where=$(cat ${line[2]}.csv | grep -n ${line[6]} | cut -d: -f1);
+# if [[ $where != 1 ]]
+# then
+#     sed -i "${where}d" ${line[2]}.csv
+# else
+#     echo -e "${ERRORTYPE}Error in deleting${NE}"
+# fi
+
+if [ ${#line[@]} -eq 3 ]
 then
-    sed -i "${where}d" ${line[2]}.csv
-else
-    echo -e "${ERRORTYPE}Error in deleting${NE}"
+echo `cat ${linel[2]}.csv | head -1` > ${line[2]}.csv
+fi
+
+if [[ ${#line[@]} -gt 3 ]]
+then 
+size=${#line[@]}
+numbers=`grep -inw ${line[6]} ${line[2]}.csv | cut -d: -f1`
+IFS=$'\n' read -rd '' -a delete_numbers <<<"$numbers"
+size=${#delete_numbers[@]}
+((j=$size-1))
+while [ $size -gt 0 ]
+do 
+
+sed -i "${delete_numbers[j]}d" ${line[2]}.csv
+((size=$size-1))
+((j=$j-1))
+done
 fi
--- scripts/deletetable.sh
+++ scripts/deletetable.sh
@@ -1,31 +1,33 @@
 #! /bin/bash
-j=0
+
+
 echo enter DELETE SQL statement
 read line 
+
+j=0
 IFS=" "; read -ra sql <<< "$line"
-#echo ${#sql[@]} 
-# test case 
-#delete from table_name 
+
 if [ ${#sql[@]} -eq 3 ]
 then
-echo `cat ${sql[2]} | head -1` > test2
+    echo `cat ${sql[2]}.csv | head -1` > ${sql[2]}.csv
 fi
+
 ###########################
 # test case 
 #delete from table_name where id = 2
 #delete from table_name where name = mohamed
 if [[ ${#sql[@]} -gt 3 ]]
 then 
-size=${#sql[@]}
-numbers=`grep -in ${sql[6]} ${sql[2]} | cut -d: -f1`
-IFS=$'\n' read -rd '' -a delete_numbers <<<"$numbers"
-size=${#delete_numbers[@]}
-((j=$size-1))
-while [ $size -gt 0 ]
-do 
-#echo ${delete_numbers[j]}
-sed -i "${delete_numbers[j]}d" ${sql[2]}
-((size=$size-1))
-((j=$j-1))
+    size=${#sql[@]}
+    numbers=`grep -inw ${sql[6]} ${sql[2]}.csv | cut -d: -f1`
+    IFS=$'\n' read -rd '' -a delete_numbers <<<"$numbers"
+    size=${#delete_numbers[@]}
+    ((j=$size-1))
+    while [ $size -gt 0 ]
+    do 
+        #echo ${delete_numbers[j]}
+        sed -i "${delete_numbers[j]}d" ${sql[2]}.csv
+        ((size=$size-1))
+        ((j=$j-1))
 done
 fi
