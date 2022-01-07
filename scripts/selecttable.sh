964a508b126344561dfabf0cb48088baa9c82502
muhammedmohab <muhammed.mohab@gmail.com>
2022-01-07 17:21:18 +0200

    Select table confirmed

--- scripts/selecttable.sh
+++ scripts/selecttable.sh
@@ -5,6 +5,7 @@ read table_name;
 i=0;
 counter=0;
 fid=0;
+condfid=0;
 
 if [[ -f ${table_name}.csv ]]
 then
@@ -29,12 +30,15 @@ then
         IFS=" "; read -ra cols <<< "$line" 
         nocols=${#cols[@]};
         ((i=$i+1))
+
         if [[ $selection == "all" && $yesorno == "n" ]]
         then
+            #print everything you have
             echo "$table_name ==============================="
             cat ${table_name}.csv | awk -F "," '{for(i=1;i<=NF;i++){if(NR==1){printf $i;printf"  "}else{printf $i;printf "\t   "}};print "\n"}'
             break;
-        else
+        elif [[ $selection == "all" && $yesorno == "y" ]]
+        then
                 echo "Enter your condition column"
                 read col;
                 echo "Enter $col value"
@@ -69,7 +73,7 @@ then
                 echo "$table_name ==============================="
                 echo -e "${CYAN}$firstline${NE}";
                 #Return record number #Searching by cols
-                recs=$(cat ${table_name}.csv | awk -F "," '{print $'$fid'}' | grep -n "$value" | cut -d: -f1)
+                recs=$(cat ${table_name}.csv | awk -F "," '{print $'$fid'}' | grep -nw "$value" | cut -d: -f1)
                 #echo "$recs";
                 IFS=$'\n' read -rd '' -a printed <<<"$recs"
                 sizeofrecs=${#printed[@]};
@@ -83,24 +87,128 @@ then
 
                 #cat ${table_name}.csv | awk -vv=$value -F "," '{for(i=1;i<NF;i++){if(NR==1){}else{if($i==v){print $i}}}}'
                 break;
-            elif [[ $selection == cols[0] ]] #if col you want to select is in the table
-            then
-                echo "$table_name ==============================="
-                
-                
-                break;
+            fi #closing the all select with condition     
+        fi #closing the all select with no condtion
+
+        if [[ $selection != "all" && $yesorno == "n" ]]
+        then
+            #echo "not all with no condtion"
+                for((k=0;k<$innersize;k++)) #Getting fields number
+                do
+                    #echo "$k this is K"
+                    line2=${items[$k]};
+                    IFS=" "; read -ra colz <<< "$line2"
+                    #echo ""
+                    #echo "Inside the for loop $k"
+                    #echo "${wordrec[$k]}, ${items[$k]} ,${colz[0]}, $col"
+                    #echo "${colz[0]}"
+                    if [[ ${colz[0]} == $selection ]]
+                    then
+                        ((fid=$k+1))
+                        #echo "inside the where condition if"
+                        break;
+                    fi
+                done
             
-            elif [[ $selection == "" ]]
+            #Handling the selection field with no condition
+            if [[ $fid == 0 ]]
             then
-                echo -e "${ERRORTYPE}You need to enter something${NE}";
-                break;
-            else
-                echo -e "${ERRORTYPE}Incorrect${NE}";
+                echo -e "${ERRORTYPE}This ${selection} not found in table${NE}"
                 break;
             fi
-        fi
-        ((size=$size-1))
-    done
+            #printing the selected field with no condition
+            echo "$table_name ==============================="
+            cat ${table_name}.csv | awk -F "," '{print $'${fid}'}';
+
+            break;
+        elif [[ $selection != "all" && $yesorno == "y" ]] 
+        then
+            echo "Enter your condition column"
+            read col;
+            echo "Enter $col value"
+            read value;
+
+            
+            #condition--------------------------------
+            
+            for((k=0;k<$innersize;k++)) #Getting fields number
+            do
+                #echo "$k this is K"
+                line2=${items[$k]};
+                IFS=" "; read -ra colz <<< "$line2"
+                #echo ""
+                #echo "Inside the for loop $k"
+                #echo "${wordrec[$k]}, ${items[$k]} ,${colz[0]}, $col"
+                #echo "${colz[0]}"
+                if [[ ${colz[0]} == $col ]]
+                then
+                    ((condfid=$k+1))
+                    #echo "$k, $condfid"
+                    #echo "inside the where condition if"
+                    break;
+                fi
+            done
+            
+            #Handling the where condition
+            if [[ $condfid == 0 ]]
+            then
+                echo -e "${ERRORTYPE}This ${selection} not found in table${NE}"
+                break;
+            fi
+
+            #selction--------------------------------
+            for((k=0;k<$innersize;k++)) #Getting fields number
+            do
+                #echo "$k this is K"
+                line2=${items[$k]};
+                IFS=" "; read -ra colz <<< "$line2"
+                #echo ""
+                #echo "Inside the for loop $k"
+                #echo "${wordrec[$k]}, ${items[$k]} ,${colz[0]}, $col"
+                #echo "${colz[0]}"
+                if [[ ${colz[0]} == $selection ]]
+                then
+                    ((fid=$k+1))
+                    #echo "$k, $fid"
+                    #echo "inside the where condition if"
+                    break;
+                fi
+            done
+            
+            #Handling the selection field with condition
+            if [[ $fid == 0 ]]
+            then
+                echo -e "${ERRORTYPE}This ${selection} not found in table${NE}"
+                break;
+            fi
+
+            echo "$table_name ==============================="
+
+            #Return record number #Searching by cols
+            recs=$(cat ${table_name}.csv | awk -F "," '{print $'$condfid'}' | grep -nw "$value" | cut -d: -f1)
+            #echo "$recs";
+            IFS=$'\n' read -rd '' -a printed <<<"$recs"
+            sizeofrecs=${#printed[@]};
+
+            counter=0;
+
+            echo -e "${CYAN}$selection${NE}"
+            while [[ sizeofrecs -gt 0 ]]
+            do
+                #echo "$fid , $condfid"
+                
+                cat ${table_name}.csv | awk -F "," '{if(NR=='${printed[counter]}'){print $'${fid}'}}'
+                ((counter=$counter+1))
+                ((sizeofrecs=$sizeofrecs-1))
+            done
+
+
+            break;
+        fi #closing of selection of not all
+        
+        ((size=$size-1)) #while loop condition
+    done #closing of while loop
+
 else
     echo -e "${ERRORTYPE}Table not found${NE}";
 fi
