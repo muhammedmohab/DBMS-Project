b68328e57422e9c43ee8a5f97762aa0e1d8c07b2
muhammedmohab <muhammed.mohab@gmail.com>
2022-01-07 20:51:49 +0200

    Final push

--- scripts/createtable.sh
+++ scripts/createtable.sh
@@ -21,33 +21,36 @@ else
     IFS=" "; read -ra colspaced <<< "$column"
     size=${#colspaced[@]};
     #echo "${size}, ${flag}"
-    if [[ (${colspaced[1]} == "int" || ${colspaced[1]} == "string") && $flag == 0 ]]
+    if [[ ${colspaced[1]} == "int" || ${colspaced[1]} == "string" ]]
     then
+        if [[ $size -lt 3 || $flag == 0 ]]
+        then
+            
+          echo -n $column >> ${table_name}.csv
+          if [[ $number -ne 1 ]]
+          then
+            echo -n , >> ${table_name}.csv
+          fi
+
+          if [[ $number -eq 1 ]]
+          then 
+            echo "">> ${table_name}.csv
+          fi
+          ((number=$number-1))
+          ((i=$i+1))
+        fi
+        if [[ $size -eq 3 && $flag == 1 ]]
+        then
+          echo -e "${ERRORTYPE}Only one PK in table${NE}";
+        fi
         if [[ $size == 3 ]]
         then
             flag=1;
             #echo "Flag changed"
         fi
-
-        echo -n $column >> ${table_name}.csv
-        if [[ $number -ne 1 ]]
-        then
-          echo -n , >> ${table_name}.csv
-        fi
-
-        if [[ $number -eq 1 ]]
-        then 
-          echo "">> ${table_name}.csv
-        fi
-        ((number=$number-1))
-        ((i=$i+1))
-    else
-        if [[ $size -gt 2 && $flag == 1 ]]
-        then
-          echo -e "${ERRORTYPE}Only one PK in table${NE}";
         else
           echo -e "${ERRORTYPE}Wrong data type (int|string)${NE}";
-        fi
+        
       fi
   done
 fi
