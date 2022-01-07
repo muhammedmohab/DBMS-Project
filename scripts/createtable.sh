2f402cefcde867a127dbf1a27dc0b7bc4570810d
muhammedmohab <muhammed.mohab@gmail.com>
2022-01-07 17:31:43 +0200

    Edited the create table to support CSV

--- scripts/createtable.sh
+++ scripts/createtable.sh
@@ -4,12 +4,12 @@ i=1
 echo "Enter table name" 
 read table_name;
 
-if [[ -f $table_name ]] # if file dont exist then continue "${table_name}.csv" 
+if [[ -f ${table_name}.csv ]] # if file dont exist then continue "${table_name}.csv" 
 then
   echo "$table_name already exists"
 else 
   
-  touch $table_name
+  touch ${table_name}.csv
   echo "Enter the number of columns"
   read number
   
@@ -18,16 +18,16 @@ else
     echo "enter the column number $i"
     read column
 
-    echo -n $column >> $table_name
+    echo -n $column >> ${table_name}.csv
 
       if [[ $number -ne 1 ]]
       then
-        echo -n , >> $table_name
+        echo -n , >> ${table_name}.csv
       fi
 
       if [[ $number -eq 1 ]]
       then 
-        echo "">> $table_name
+        echo "">> ${table_name}.csv
       fi
       ((number=$number-1))
       ((i=$i+1))
